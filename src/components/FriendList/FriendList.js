1ac89985712f896c7cec7746a71f6b909e09296e
Eugene Hlushak <hlushak.e@gmail.com>
2023-02-11 11:38:18 +0200

    add css to Profile

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 
 export default function FriendList({ friends }) {
   return (
-    <ul class="friend-list">
+    <ul className="friend-list">
       {friends.map(({ id, avatar, name, isOnline }) => {
         return (
-          <li class="item" key={id}>
-            <span class="status">{isOnline}</span>
-            <img class="avatar" src={avatar} alt="User avatar" width="48" />
-            <p class="name">{name}</p>
+          <li className="item" key={id}>
+            <span className="status">{isOnline}</span>
+            <img className="avatar" src={avatar} alt="User avatar" width="48" />
+            <p className="name">{name}</p>
           </li>
         );
       })}
